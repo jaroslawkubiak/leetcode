b1209686cbdaf1c7046d57e02341f1710598cbba
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-08-08 21:29:22 +0200

    finish number palindrome

--- palindrome number/script.js
+++ palindrome number/script.js
@@ -1,3 +1,9 @@
 "use strict";
+const x = -121;
 
-
+const palindromeNumber = function (x) {
+  const palindrome = x.toString();
+  const reverseString = palindrome.split("").reverse().join("");
+  return palindrome === reverseString ? true : false;
+};
+console.log(palindromeNumber(x));
